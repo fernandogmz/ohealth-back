c9050d7d52a298200a1d0a57d2ff60126b51f276
Fernando Javier Gómez <j_gomez_20@hotmail.com>
2019-11-12 02:29:56 -0600

    CORS ENABLE 0.2

--- index.php
+++ index.php
@@ -9,16 +9,17 @@
 </body>
 </html>
 <?php
-	header('Access-Control-Allow-Origin: *');
-header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
-header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
-header("Allow: GET, POST, OPTIONS, PUT, DELETE");
-$method = $_SERVER['REQUEST_METHOD'];
-if($method == "OPTIONS") {
-    die();
-}
+
 
 	require_once('connection.php');
+	header('Access-Control-Allow-Origin: *');
+	header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+	header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+	header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+	$method = $_SERVER['REQUEST_METHOD'];
+	if($method == "OPTIONS") {
+    		die();
+	}
 
 	$result = array(
 		'status'	=>	'ok',
