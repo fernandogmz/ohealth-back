b6ca13933b5a3204cc65bba28197d96297d27709
Fernando Javier Gómez <j_gomez_20@hotmail.com>
2019-11-12 02:27:57 -0600

    Updated CORS

--- index.php
+++ index.php
@@ -9,7 +9,15 @@
 </body>
 </html>
 <?php
-	
+	header('Access-Control-Allow-Origin: *');
+header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
+header("Allow: GET, POST, OPTIONS, PUT, DELETE");
+$method = $_SERVER['REQUEST_METHOD'];
+if($method == "OPTIONS") {
+    die();
+}
+
 	require_once('connection.php');
 
 	$result = array(
